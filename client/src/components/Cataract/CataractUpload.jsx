68758d6e90e63b76a6d96f53c79baf6c716b4062
ShenanPerera <shenaperera2001@gmail.com>
2023-10-15 10:12:57 +0530

    feat:validation

--- client/src/components/Cataract/CataractUpload.jsx
+++ client/src/components/Cataract/CataractUpload.jsx
@@ -1,14 +1,241 @@
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+// import { allowedExtensions } from "../../utils/regex";
+// import Swal from "sweetalert2";
+
+// const CataractUpload = () => {
+//   const [file, setFile] = useState(null);
+//   const [fetchResult, setFetchResult] = useState(null);
+//   const navigate = useNavigate();
+//   const [predictionText, setPredictionText] = useState(null);
+
+
+//   const handleFileChange = (e) => {
+//     const selectedFile = e.target.files[0];
+//     if (!allowedExtensions.exec(selectedFile.name)) {
+//       Swal.fire({
+//         icon: "error",
+//         title: "Invalid image type",
+//         text: "Please upload a SVG, PNG, JPG or JPEG file",
+//         confirmButtonColor: "#DC2626",
+//       });
+//     } else {
+//       console.log("File selected:", selectedFile);
+//       setFile(selectedFile);
+//       setIsFileSet(true);
+//     }
+//   };
+
+//   const handleFundusDetection = async (image) => {
+//     const formData = new FormData();
+//     formData.append('image', image);
+
+//     const response = await fetch('/api/detect/fundus', {
+//       method: 'POST',
+//       body: formData,
+//     });
+
+//     console.log('submit');
+
+//     if (!response.ok) {
+//       alert('Error detecting fundus');
+//     } else {
+//       const data = await response.json();
+
+//       // Check if the image is detected as a fundus before sending it for cataract prediction
+//       if (data.is_fundus) {
+//         Swal.fire({
+//           title:'Fundus Detected',
+//           text: 'Uploaded image is a fundus',
+//           icon: 'success',
+//           showConfirmButton: false,
+//           timer: 2000,
+//           timerProgressBar: true
+//         })
+//         handleSubmit(image);
+//       } else {
+//         Swal.fire({
+//           title:'Try Again',
+//           text: 'Uploaded image is not a fundus',
+//           icon: 'error',
+//           showConfirmButton: false,
+//           timer: 2000,
+//           timerProgressBar: true
+//       })
+//       navigate(`/getstarted`);
+//       }
+//     }
+//   };
+
+// //   const handleCataractPrediction = async (image) => {
+// //     const formData = new FormData();
+// //     formData.append('image', image);
+
+// // =======
+
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+  
+//     const formData = new FormData();
+//     formData.append("image", file);
+ 
+//     const response = await fetch("/api/predict/cataract", {
+//       method: "POST",
+//       body: formData,
+//     });
+
+//     if (!response.ok) {
+//       throw new Error("Error uploading file");
+//     }
+
+//     const data = await response.json();
+//     console.log("Response from server:", data);
+//     setFetchResult(data);
+//   } 
+
+//   const handleCataract = async (e) => {
+//     if (file) {
+//       handleFundusDetection(file);
+//     } else {
+//       alert('No file selected.');
+  
+//     if (!response.ok) {
+//       Swal.fire({
+//         icon: "error",
+//         title: "Error uploading file",
+//         text: "Please try again later",
+//         confirmButtonColor: "rgb(234 88 12)",
+//       });
+//       throw new Error("Error uploading file");
+//     }
+  
+//     if (response.ok) {
+//       const data = await response.json();
+//       console.log("Response from server:", data);
+  
+//       let predictionText = "error"; // Default value
+  
+//       if (data.predictions < 0.5) {
+//         predictionText = "Normal";
+//       } else if (data.predictions >= 0.5 && data.predictions < 0.6) {
+//         predictionText = "Mild Cataract";
+//       } else if (data.predictions >= 0.6 && data.predictions < 0.7) {
+//         predictionText = "Moderate Cataract";
+//       } else if (data.predictions >= 0.7) {
+//         predictionText = "Severe Cataract";
+//       }
+  
+//       setPredictionText(predictionText);
+//       setFetchResult(data);
+  
+//       Swal.fire({
+//         icon: "success",
+//         title: "Prediction Score : " + data.predictions + "\n" + predictionText,
+//         text: "Please contact a doctor for further diagnosis",
+//         width: "50rem",
+//         confirmButtonColor: "rgb(234 88 12)",
+//       });
+//     }
+//   };
+  
+
+//   return (
+//     <div className="my-10 justify-self-center p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 lg:w-[500px] md:w-[400px] w-[90vw]">
+//       <h4 className="mb-12 text-2xl font-semibold text-white flex justify-center">
+//         Cataract detection
+//       </h4>
+//       <div className="flex items-center justify-center min-w-min">
+//         <label
+//           htmlFor="dropzone-file"
+//           className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+//         >
+//           <div className="flex flex-col items-center justify-center pt-5 pb-6">
+//             <svg
+//               className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+//               aria-hidden="true"
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 20 16"
+//             >
+//               <path
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+//               />
+//             </svg>
+//             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+//               <span className="font-semibold">Click to upload</span> or drag and
+//               drop
+//             </p>
+//             <p className="text-xs text-gray-500 dark:text-gray-400">
+//               PNG or JPG
+//             </p>
+//           </div>
+//           <input
+//             id="dropzone-file"
+//             type="file"
+//             className="hidden"
+//             onChange={handleFileChange}
+//             accept="image/*"
+//           />
+//         </label>
+//       </div>
+
+//       <div className="flex justify-center">
+//         {file && (
+//           <div className="w-1/2 bg-white border border-gray-200 rounded-lg shadow m-12 p-5">
+//             {file && (
+//               <img
+//                 src={URL.createObjectURL(file)}
+//                 alt="Uploaded"
+//                 className="rounded-lg"
+//               />
+//             )}
+//           </div>
+//         )}
+//       </div>
+
+//       <div className="flex justify-center">
+//         {file && (
+//           <button
+//             type="submit"
+//             className="w-80 rounded-full bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+//             onClick={handleCataract}
+//           >
+//             Submit
+//           </button>
+//         )}
+//       </div>
+//       <div className="text-white flex justify-center mt-2">
+//         {
+//         // isLoading ? (
+//         //   <p>Loading...</p>
+//         // ) : (
+//           fetchResult && (
+//             <div>
+//               <p>Prediction: {fetchResult.predictions[0][0]}</p>
+//             </div>
+//           )
+//         // )
+//         }
+//       </div>
+//     </div>
+//   );
+// }};
+
+// export default CataractUpload;
+
 import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import { allowedExtensions } from "../../utils/regex";
 import Swal from "sweetalert2";
 
 const CataractUpload = () => {
   const [file, setFile] = useState(null);
   const [fetchResult, setFetchResult] = useState(null);
-  const navigate = useNavigate();
   const [predictionText, setPredictionText] = useState(null);
-
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -26,9 +253,9 @@ const CataractUpload = () => {
     }
   };
 
-  const handleFundusDetection = async (image) => {
+  const handleFundusDetection = async () => {
     const formData = new FormData();
-    formData.append('image', image);
+    formData.append('image', file);
 
     const response = await fetch('/api/detect/fundus', {
       method: 'POST',
@@ -38,67 +265,45 @@ const CataractUpload = () => {
     console.log('submit');
 
     if (!response.ok) {
-      alert('Error detecting fundus');
+      alert('Error uploading file');
     } else {
       const data = await response.json();
+      console.log(data)
+      
+      if(data.is_fundus == true){
+      Swal.fire({
+        title: 'Fundus Detected',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true
+    })
+    handleSubmit(file)
+  }
+    else{
+      Swal.fire({
+        title: 'Not a fundus.Try Again!',
+        icon: 'error',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true
+    })
+    }
 
-      // Check if the image is detected as a fundus before sending it for cataract prediction
-      if (data.is_fundus) {
-        Swal.fire({
-          title:'Fundus Detected',
-          text: 'Uploaded image is a fundus',
-          icon: 'success',
-          showConfirmButton: false,
-          timer: 2000,
-          timerProgressBar: true
-        })
-        handleSubmit(image);
-      } else {
-        Swal.fire({
-          title:'Try Again',
-          text: 'Uploaded image is not a fundus',
-          icon: 'error',
-          showConfirmButton: false,
-          timer: 2000,
-          timerProgressBar: true
-      })
-      navigate(`/getstarted`);
-      }
     }
   };
 
-//   const handleCataractPrediction = async (image) => {
-//     const formData = new FormData();
-//     formData.append('image', image);
 
-// =======
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
+    // e.preventDefault();
   
     const formData = new FormData();
     formData.append("image", file);
- 
+  
     const response = await fetch("/api/predict/cataract", {
       method: "POST",
       body: formData,
     });
-
-    if (!response.ok) {
-      throw new Error("Error uploading file");
-    }
-
-    const data = await response.json();
-    console.log("Response from server:", data);
-    setFetchResult(data);
-  } 
-
-  const handleCataract = async (e) => {
-    if (file) {
-      handleFundusDetection(file);
-    } else {
-      alert('No file selected.');
   
     if (!response.ok) {
       Swal.fire({
@@ -203,27 +408,15 @@ const CataractUpload = () => {
           <button
             type="submit"
             className="w-80 rounded-full bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-            onClick={handleCataract}
+            onClick={handleFundusDetection}
           >
             Submit
           </button>
         )}
-      </div>
-      <div className="text-white flex justify-center mt-2">
-        {
-        // isLoading ? (
-        //   <p>Loading...</p>
-        // ) : (
-          fetchResult && (
-            <div>
-              <p>Prediction: {fetchResult.predictions[0][0]}</p>
-            </div>
-          )
-        // )
-        }
       </div>
     </div>
   );
 };
 
 export default CataractUpload;
+
--- client/src/components/DiabeticRetinopathy/DRUpload.jsx
+++ client/src/components/DiabeticRetinopathy/DRUpload.jsx
@@ -1,14 +1,269 @@
+// import { useState } from 'react';
+// // import { Link } from 'react-router-dom';
+// import { allowedExtensions } from '../../utils/regex';
+// import Swal from 'sweetalert2';
+// import { useNavigate } from 'react-router-dom';
+
+// const DRUpload = () => {
+//   const [file, setFile] = useState(null);
+//   // const [data, setData] = useState(null);
+//   const navigate = useNavigate();
+
+
+//   const handleFileChange = (e) => {
+//     const selectedFile = e.target.files[0];
+//     if (!allowedExtensions.exec(selectedFile.name)) {
+//       Swal.fire({
+//         icon: 'error',
+//         title: 'Invalid image type',
+//         text: 'Please upload a SVG, PNG, JPG or JPEG file',
+//         confirmButtonColor: '#DC2626',
+//       });
+//     } else {
+//       console.log('File selected:', selectedFile);
+//       setFile(selectedFile);
+//     }
+//   };
+
+//   const handleResponse = () => {
+//     setFile(null);
+//   };
+
+//   const handleFundusDetection = async (image) => {
+//     const formData = new FormData();
+//     formData.append('image', image);
+
+//     const response = await fetch('/api/detect/fundus', {
+//       method: 'POST',
+//       body: formData,
+//     });
+
+//     console.log('submit');
+
+//     if (!response.ok) {
+//       alert('Error detecting fundus');
+//     } else {
+//       const data = await response.json();
+
+//       // Check if the image is detected as a fundus before sending it for DR prediction
+//       if (data.is_fundus) {
+//         Swal.fire({
+//           title:'Fundus Detected',
+//           text: 'Uploaded image is a fundus',
+//           icon: 'success',
+//           showConfirmButton: false,
+//           timer: 2000,
+//           timerProgressBar: true
+//         })
+//         handleSubmit(image);
+//       } else {
+//         Swal.fire({
+//           title:'Try Again',
+//           text: 'Uploaded image is not a fundus',
+//           icon: 'error',
+//           showConfirmButton: false,
+//           timer: 2000,
+//           timerProgressBar: true
+//       })
+//       navigate(`/getstarted`);
+//       }
+//     }
+//   };
+
+// //   const handleDRPrediction = async (image) => {
+
+
+//   const handleSecondOpinion = async () => {
+//     const formData = new FormData();
+//     formData.append('image', file);
+
+//     const response = await fetch('/api/predict/dr', {
+//       method: 'POST',
+//       body: formData,
+//     });
+
+//     if (!response.ok) {
+//       Swal.fire({
+//         icon: 'error',
+//         title: 'Error uploading file',
+//         text: 'Please try again later',
+//         confirmButtonColor: 'rgb(234 88 12)',
+//       });
+//     } else {
+//       const temp = await response.json();
+//       console.log('Response from server:', temp);
+
+//       Swal.fire({
+//         icon:
+//           temp.predictions === 'Mild Diabetic Retinopathy' ||
+//           temp.predictions === 'Moderate Diabetic Retinopathy' ||
+//           temp.predictions === 'Severe Diabetic Retinopathy' ||
+//           temp.predictions === 'Proliferate Diabetic Retinopathy'
+//             ? 'warning'
+//             : 'success',
+//         title: 'Second result : \n' + temp.predictions,
+//         text: temp.confidence && 'Confidence: ' + temp.confidence * 100 + '%',
+//         width: '50rem',
+//         showConfirmButton: false,
+//         showDenyButton: true,
+//         showCancelButton: true,
+//         confirmButtonColor: 'rgb(234 88 12)',
+//         denyButtonText: 'Re-upload',
+//         cancelButtonText: 'Go to Dashboard',
+//       }).then((result) => {
+//         if (result.isDenied) {
+//           handleResponse();
+//         } else {
+//           window.location.href = '/getstarted';
+//         }
+//       });
+//     };
+// //   }
+
+//   const handleSubmit = async () => {
+//     const formData = new FormData();
+//     formData.append('image', file);
+
+//     const response = await fetch('/api/predict/dr2', {
+//       method: 'POST',
+//       body: formData,
+//     });
+
+//     if (!response.ok) {
+//       Swal.fire({
+//         icon: 'error',
+//         title: 'Error uploading file',
+//         text: 'Please try again later',
+//         confirmButtonColor: 'rgb(234 88 12)',
+//       });
+//     } else {
+//       const temp = await response.json();
+//       console.log('Response from server:', temp);
+
+//       Swal.fire({
+//         icon:
+//           temp.predictions === 'Mild Diabetic Retinopathy' ||
+//           temp.predictions === 'Moderate Diabetic Retinopathy' ||
+//           temp.predictions === 'Severe Diabetic Retinopathy' ||
+//           temp.predictions === 'Proliferate Diabetic Retinopathy'
+//             ? 'warning'
+//             : 'success',
+//         title: 'Result : \n' + temp.predictions,
+//         text: 'Please contact a doctor for further diagnosis',
+//         width: '50rem',
+//         showDenyButton: true,
+//         showConfirmButton: true,
+//         showCancelButton: true,
+//         confirmButtonColor: 'rgb(234 88 12)',
+//         confirmButtonText: 'Get result using another model',
+//         denyButtonText: 'Re-upload',
+//         cancelButtonText: 'Go to Dashboard',
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//           handleSecondOpinion();
+//         } else if (result.isDenied) {
+//           handleResponse();
+//         } else {
+//           window.location.href = '/getstarted';
+//         }
+//       });
+//     }
+//   };
+
+//   const handleResponse = () => {
+//     setData(null);
+//     setFile(null);
+//   };
+
+//   const handleDR = async () => {
+//     if (file) {
+//       handleFundusDetection(file);
+//     } else {
+//       alert('No file selected.');
+//     }
+//   };
+
+//   return (
+//     <div className="justify-self-center w-7/12 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-5">
+//       <h4 className="mb-12 text-2xl font-semibold text-white flex justify-center">
+//         Diabetic Retinopathy detection
+//       </h4>
+//       <div className="flex items-center justify-center min-w-min">
+//         <label
+//           htmlFor="dropzone-file"
+//           className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+//         >
+//           <div className="flex flex-col items-center justify-center pt-5 pb-6">
+//             <svg
+//               className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+//               aria-hidden="true"
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 20 16"
+//             >
+//               <path
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+//               />
+//             </svg>
+//             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+//               <span className="font-semibold">Click to upload</span> or drag and
+//               drop
+//             </p>
+//             <p className="text-xs text-gray-500 dark:text-gray-400">
+//               SVG, PNG, JPG or JPEG (MAX. 800x400px)
+//             </p>
+//           </div>
+//           <input
+//             id="dropzone-file"
+//             type="file"
+//             className="hidden"
+//             onChange={handleFileChange}
+//           />
+//         </label>
+//       </div>
+
+//       <div className="flex justify-center">
+//         {file && (
+//           <div className=" bg-white border border-gray-200 rounded-lg shadow m-9 p-3">
+//             {file && (
+//               <img
+//                 src={URL.createObjectURL(file)}
+//                 alt="Uploaded"
+//                 className="rounded-lg w-80"
+//               />
+//             )}
+//           </div>
+//         )}
+//       </div>
+
+//       <div className="flex justify-center">
+//         {file && (
+//           <button
+//             type="submit"
+//             className="w-80 rounded-full bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+//             onClick={handleDR}
+//           >
+//             Submit
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }};
+
+// export default DRUpload;
+
+
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { allowedExtensions } from '../../utils/regex';
 import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
 
 const DRUpload = () => {
   const [file, setFile] = useState(null);
-  const [data, setData] = useState(null);
-  const navigate = useNavigate();
-
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -25,52 +280,9 @@ const DRUpload = () => {
     }
   };
 
-  const handleFundusDetection = async (image) => {
-    const formData = new FormData();
-    formData.append('image', image);
-
-    const response = await fetch('/api/detect/fundus', {
-      method: 'POST',
-      body: formData,
-    });
-
-    console.log('submit');
-
-    if (!response.ok) {
-      alert('Error detecting fundus');
-    } else {
-      const data = await response.json();
-
-      // Check if the image is detected as a fundus before sending it for DR prediction
-      if (data.is_fundus) {
-        Swal.fire({
-          title:'Fundus Detected',
-          text: 'Uploaded image is a fundus',
-          icon: 'success',
-          showConfirmButton: false,
-          timer: 2000,
-          timerProgressBar: true
-        })
-        handleSubmit(image);
-      } else {
-        Swal.fire({
-          title:'Try Again',
-          text: 'Uploaded image is not a fundus',
-          icon: 'error',
-          showConfirmButton: false,
-          timer: 2000,
-          timerProgressBar: true
-      })
-      navigate(`/getstarted`);
-      }
-    }
+  const handleResponse = () => {
+    setFile(null);
   };
-
-//   const handleDRPrediction = async (image) => {
-
-//   const handleResponse = () => {
-//     setFile(null);
-//   };
 
   const handleSecondOpinion = async () => {
     const formData = new FormData();
@@ -116,8 +328,8 @@ const DRUpload = () => {
           window.location.href = '/getstarted';
         }
       });
-    };
-//   }
+    }
+  };
 
   const handleSubmit = async () => {
     const formData = new FormData();
@@ -169,16 +381,44 @@ const DRUpload = () => {
     }
   };
 
-  const handleResponse = () => {
-    setData(null);
-    setFile(null);
-  };
+  
+  const handleFundusDetection = async () => {
+    const formData = new FormData();
+    formData.append('image', file);
 
-  const handleDR = async () => {
-    if (file) {
-      handleFundusDetection(file);
+    const response = await fetch('/api/detect/fundus', {
+      method: 'POST',
+      body: formData,
+    });
+
+    console.log('submit');
+
+    if (!response.ok) {
+      alert('Error uploading file');
     } else {
-      alert('No file selected.');
+      const data = await response.json();
+      console.log(data)
+      
+      if(data.is_fundus == true){
+      Swal.fire({
+        title: 'Fundus Detected',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true
+    })
+    handleSubmit(file)
+  }
+    else{
+      Swal.fire({
+        title: 'Not a fundus.Try Again!',
+        icon: 'error',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true
+    })
+    }
+
     }
   };
 
@@ -244,7 +484,7 @@ const DRUpload = () => {
           <button
             type="submit"
             className="w-80 rounded-full bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-            onClick={handleDR}
+            onClick={handleFundusDetection}
           >
             Submit
           </button>
